dc5558b091112d97e3a618d78f8535b2ba305043
Jan <kupper.thorge@gmail.com>
2025-03-08 15:17:11 +0100

    dots

--- components/DotPlain.js
+++ components/DotPlain.js
@@ -5,7 +5,13 @@ import {
   OrbitControls,
 } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef, useEffect } from "react";
+import {
+  Noise,
+  EffectComposer,
+  TiltShift2,
+  DepthOfField,
+} from "@react-three/postprocessing";
+import { useRef } from "react";
 import { MathUtils } from "three";
 
 const rows = 100;
@@ -14,20 +20,23 @@ const columns = 100;
 const particles = Array.from({ length: rows * columns }, (_, index) => ({
   factor: MathUtils.randInt(20, 100),
   speed: MathUtils.randFloat(0.01, 0.75),
-  xFactor: (index % columns) * 1 - columns * 0.5,
-  zFactor: MathUtils.randFloatSpread(1),
-  yFactor: (Math.floor(index / columns) - 5) * 1,
+  xFactor: (index % columns) * 0.5 - columns * 0.25,
+  yFactor: MathUtils.randFloatSpread(1),
+  zFactor: (Math.floor(index / columns) - 5) * 0.5,
+  size: MathUtils.randFloat(0.02, 0.04),
 }));
 
 export default function DotPlain() {
   return (
     <div className="h-screen w-screen">
-      <Canvas shadows camera={{ position: [0, 0, 50], fov: 50 }}>
-        <color attach="background" args={["#161616"]} />
-        <fog attach="fog" args={["red", 10, 200]} />
-        <Environment preset="sunset" />
-        <OrbitControls />
+      <Canvas camera={{ position: [0, 0, 60], fov: 50 }}>
+        <color attach="background" args={["#e2e2e2"]} />
         <Dots />
+        <EffectComposer>
+          {/* <TiltShift2 blur={0.05} /> */}
+          {/* <Noise opacity={0.2} /> */}
+          <DepthOfField focusDistance={0} focalLength={0.5} bokehScale={2} />
+        </EffectComposer>
       </Canvas>
     </div>
   );
@@ -37,30 +46,28 @@ function Dots() {
   const ref = useRef();
 
   useFrame((state, delta) => {
-    // ref.current.rotation.y = MathUtils.damp(
-    //   ref.current.rotation.y,
-    //   -state.mouse.x * 0.2,
-    //   2,
-    //   delta,
-    // );
+    ref.current.rotation.y = MathUtils.damp(
+      ref.current.rotation.y,
+      -state.mouse.x * 0.02,
+      4,
+      delta,
+    );
 
-    // ref.current.rotation.x = MathUtils.damp(
-    //   ref.current.rotation.x,
-    //   -state.mouse.y * 0.2,
-    //   2,
-    //   delta,
-    // );
+    ref.current.rotation.x = MathUtils.damp(
+      ref.current.rotation.x,
+      state.mouse.y * 0.02,
+      4,
+      delta,
+    );
 
-    ref.current.rotation.x = 100;
-
-    ref.current.position.y = -40;
-    ref.current.position.z = 50;
+    // ref.current.position.y = -40;
+    // ref.current.position.z = 50;
 
     ref.current.children.forEach((child, index) => {
       const time = state.clock.getElapsedTime();
-      const yOffset = Math.sin(time * 5 + index * 0.1) * 0.1;
-      const xOffset = Math.sin(time * 2 + index * 0.5) * 1.5;
-      const zOffset = Math.sin(time * 1 + index * 0.5) * 1;
+      const yOffset = Math.sin(time * 0.5 + index * 0.5) * 0.2;
+      const xOffset = Math.sin(time * 0.5 + index * 0.5) * 0.3;
+      const zOffset = Math.sin(time * 0.5 + index * 0.5) * 0.4;
 
       child.position.y = particles[index].yFactor + yOffset;
       child.position.x = particles[index].xFactor + xOffset;
@@ -70,13 +77,13 @@ function Dots() {
 
   return (
     <Instances ref={ref} limit={particles.length}>
-      <sphereGeometry args={[0.03, 64, 64]} />
-      <meshStandardMaterial color={"orange"} roughness={1} />
+      <sphereGeometry args={[1, 64, 64]} />
+      <meshBasicMaterial color={"#525252"} roughness={1} />
       {particles.map((particle, index) => (
         <Instance
           key={index}
+          scale={[particle.size, particle.size, particle.size]}
           position={[particle.xFactor, particle.yFactor, particle.zFactor]}
-          rotation={[0, MathUtils.degToRad(particle.factor), 0]}
         />
       ))}
     </Instances>
